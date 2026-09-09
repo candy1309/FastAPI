858f9d860379ee77e6af72de7b6ff7917a2e5433
SAGAR <candysagarmusic@gmail.com>
2026-09-09 16:09:44 +0530

    Refactor FastAPI application and remove authentication

--- main.py
+++ main.py
@@ -1,55 +1,17 @@
-from fastapi import FastAPI,HTTPException,Depends,Header
-from jose import jwt
-from datetime import datetime, timedelta, timezone
+from fastapi import FastAPI
+
 
 app = FastAPI()
 
-SECRET_KEY = "mysecret"
 
-ALGORITHM = "HS256"
-
-#Create Token
-def create_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=30)
-    to_encode.update({
-        "exp":expire
-    })
-    token = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
-
-    return token
-
-#Login API(Token Genrate)
-@app.post("/login")
-def login(username:str,password:str):
-    if username != "admin" or password != "1234":
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid Username and password"
-        )
-    token = create_token({
-        "sub":username
-    })
+@app.get("/")
+def home():
     return{
-        "access_token": token
+        "message":"Hello Mohit"
     }
 
-#Token Varify
-def varify_token(token: str = Header(None)):
-    
-    try:
-        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
-        return payload
-    except:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid or expired Token"
-        )
-    
-#protected Route
-@app.get("/secure")
-def secure_data(user = Depends(varify_token)):
+@app.get("/add")
+def add(a:int, b:int):
     return{
-        "message":"Secure Data Accessed",
-        "user":user
+        "result": a+b
     }
